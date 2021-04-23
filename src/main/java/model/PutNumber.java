7a1e55f83909214faedbea645af48c30905dfd6d
“Asumi <shimada.asumi@gmail.com>
2021-04-23 22:14:35 +0900

    一回作り直しました

--- src/main/java/model/PutNumber.java
+++ src/main/java/model/PutNumber.java
@@ -1,24 +1,8 @@
 package model;
 
-import java.util.List;
-
-/*
- *　1．0が入力されているマスを探す。
- *　2．重複をチェックし、順番に入れることのできる数字を置く。
- *　3．途中で、入れることのできる数字がなくなれば、一つ前に戻り、数字を入れなおす。
- *　4．全部埋まったときに結果を返す。
- */
-
 public class PutNumber {
-	public String[][] putNumber(List<Sudoku>numberList) {
-		
-		//	答え用の配列
-		String[][] answer = new String[9][9];
-		
-		//　数独のデータを二次元配列に格納する
-		SetNumber setNumber = new SetNumber();
-		String[][] sudoku = setNumber.setNumber(numberList);
-		
-		return answer;
+	public String[][] putNumber(String[][] sd2D){
+		String[][] answer2D = new String[9][9];
+		return answer2D;
 	}
 }
